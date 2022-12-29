86f7ff3665d73d90ee600d62490c9e310a5ad70c
Bronson Wardle <bronson.wardle@gmail.com>
2022-12-28 22:11:36 -0500

    added countdown timer and timer decreases when answer incorrectly. Also added score submit button and scores being stored in the local storage.

--- Assets/script.js
+++ Assets/script.js
@@ -44,9 +44,13 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitButton = document.getElementById("submit");
+const leaderBoard = document.getElementById("leaderBoard");
 
 let currentQuiz = 0;
 let score = 0;
+let timer = 30;
+let interval;
+
 
 // Pull each answer element and when checked it deselects.
 const deselectAnswers = () => {
@@ -71,17 +75,60 @@ const deselectAnswers = () => {
     d_text.innerText = currentQuizData.d;
   };
   loadQuiz();
+
+const startTimer = () => {
+  interval = setInterval(() => {
+    timer--;
+    document.getElementById("time").innerText = timer;
+    if (timer <= 0) {
+      clearInterval(interval);
+      quiz.innerHTML = `
+        <h2>Time's up! You answered ${score}/${quizData.length} questions correctly</h2>
+        <button onclick="history.go(0)">Play Again</button>
+      `;
+    }
+  }, 1000);
+};
+
+const submitI = () => {
+  // Retrieve the value of the initials input field
+  const initials = document.getElementById("initials").value;
+
+  // Save the score and initials to local storage
+  localStorage.setItem("score", score);
+  localStorage.setItem("initials", initials);
+
+  // Display a message to confirm that the score and initials have been saved
+  alert(`Your score of ${score} and initials "${initials}" have been saved!`);
+};
+
 submitButton.addEventListener("click", () => {
 const answer = getSelected();
 if (answer) {
-if (answer === quizData[currentQuiz].correct) score++;
+if (answer === quizData[currentQuiz].correct) 
+{score++; 
+} else{timer = timer - 5 }
+} 
+
 currentQuiz++;
 if (currentQuiz < quizData.length) loadQuiz();
 else {
 quiz.innerHTML = `
 <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-<button onclick="history.go(0)">Play Again</button>
+<button onclick="history.go(0)">Play Again</button> 
+<br>
+  Enter your initials: <input type="text" id="initials"></input>
+<button id="submitInitials">Submit</button>
 ` // location.reload() won't work in CodePen for security reasons;
+
+const submitInitialsButton = document.getElementById("submitInitials");
+submitInitialsButton.addEventListener("click", submitI);
 }
 }
-});
+);
+
+
+
+startTimer();
+
+
